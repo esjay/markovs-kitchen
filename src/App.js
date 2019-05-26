89b8e01af1d0f4709340171913d07f25ee7f666b
esjay <wayne.elgin@gmail.com>
2019-05-25 23:33:51 -0600

    Move generate code to own module

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SeedText = () => <>{'Start '.repeat(9999)}</>
+
+const App = ({}) => (
+    <SeedText/>
+);
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,30 +1,5 @@
-// const MarkovGen = require('markov-generator');
-// const fs = require('fs');
+import React from 'react';
+import {render} from 'react-dom';
+import App from './App';
 
-// const init = () => {
-//   try {
-//     const input = fs.readFileSync('data/smitten.txt').toString().split("\n");
-//     // console.log(input)
-//     const markov = new MarkovGen({
-//       input,
-//       minLength: 10
-//     });
-
-//     const sentence = markov.makeChain();
-//     console.log(sentence);
-//   } catch (e) {
-//     console.error(e);
-//   }
-// }
-
-// init();
-const React = require('react');
-const ReactDOM = require('react-dom');
-// const Gen = require('sentence-generator');
-
-// const gen = Gen('data/smitten.txt');
-// console.log(gen.take(6));
-const App = ({}) => (
-    <div>Test</div>
-);
-ReactDOM.render(<App />, document.querySelector('#root'));
+render(<App />, document.querySelector('#root'));
