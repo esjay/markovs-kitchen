5b61eac69f8191631f538b1b47d54774ab69eaab
esjay <wayne.elgin@gmail.com>
2019-05-25 22:14:56 -0600

    Use new generator lib

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
-const MarkovGen = require('markov-generator');
-const fs = require('fs');
+// const MarkovGen = require('markov-generator');
+// const fs = require('fs');
 
-const init = () => {
-  try {
-    const input = fs.readFileSync('data/smitten.txt').toString().split("\n");
-    // console.log(input)
-    const markov = new MarkovGen({
-      input,
-      minLength: 10
-    });
+// const init = () => {
+//   try {
+//     const input = fs.readFileSync('data/smitten.txt').toString().split("\n");
+//     // console.log(input)
+//     const markov = new MarkovGen({
+//       input,
+//       minLength: 10
+//     });
 
-    const sentence = markov.makeChain();
-    console.log(sentence);
-  } catch (e) {
-    console.error(e);
-  }
-}
+//     const sentence = markov.makeChain();
+//     console.log(sentence);
+//   } catch (e) {
+//     console.error(e);
+//   }
+// }
 
-init();
+// init();
+
+const Gen = require('sentence-generator');
+
+const gen = Gen('data/smitten.txt');
+console.log(gen.take(6));
