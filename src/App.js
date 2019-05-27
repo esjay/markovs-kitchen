c6bea8023beac0bebc6ccd2fee6edf8e6387f9e2
esjay <wayne.elgin@gmail.com>
2019-05-27 01:38:47 -0600

    Get some working in-page chain text

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import React, {useState} from 'react';
+import React, {useState, useLayoutEffect} from 'react';
 import { Waypoint } from 'react-waypoint';
+import {Chain, mockGen} from './Chain';
 
-
-const SeedText = () => <>{'Begin '.repeat(999)}</>
-const EndText = () => <>{'End '.repeat(999)}</>
+const ChainParagraph = ({numSentences = 5}) => {
+    const [text, setText] = useState(Chain.bind(this, {numSentences}));
+    return (<p>{text}</p>);
+};
 
 const App = () => {
     const [middle, setMiddle] = useState('');
     const [length, setLength] = useState(0);
-    return <>
-        <SeedText/>
-        <span>{middle.repeat(length)}</span>
-        <Waypoint
-            bottomOffset={'-30%'}
-            onEnter={({waypointTop}) => {setMiddle('Middle '); setLength(waypointTop*10);}}
-            debug={false}
-        >
-        </Waypoint>
-        <EndText/>
-    </>
+    useLayoutEffect(() => {
+
+    });
+    return (
+        <>
+            <ChainParagraph numSentences={10}/>
+            <span>{middle.repeat(length)}</span>
+            <Waypoint
+                bottomOffset={'-30%'}
+                onEnter={({waypointTop}) => {setMiddle(mockGen); setLength(waypointTop*10);}}
+                debug={true}
+            >
+            </Waypoint>
+            <ChainParagraph numSentences={10}/>
+        </>
+    );
 };
 
 export default App;
---
+++ src/Chain.js
@@ -0,0 +1,18 @@
+import MarkovGen from 'markov-generator';
+import smittenTxt from './data/smitten';
+
+const markov = new MarkovGen({
+    input: smittenTxt
+  });
+
+export const Chain = ({minLength = 10, numSentences = 1}) => {
+    return Array.from({ length: numSentences }).reduce((str) => {
+        return str + ' ' + markov.makeChain(minLength);
+    }, '');
+};
+
+export const mockGen = () => {
+    return 'middle ';
+};
+
+export default mockGen;
